1c6312fb13c45b4e98bc3c5dc973a609c22c2017
Roy Shenbaum <roy@delasport.com>
2018-05-01 10:44:52 +0100

    Semantic UI setup

--- group-poll/src/index.js
+++ group-poll/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,
+    Route,
+    Link} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
